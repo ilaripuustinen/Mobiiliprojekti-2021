ece2d0d7bf91b78a37296d698e49f698788eea52
n9puev00 <n9puev00@students.oamk.fi>
2021-11-09 17:31:15 +0200

    ehkä toimii ehkä ei

--- components/Planet.js
+++ components/Planet.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, SectionList } from 'react-native';
 
 export default function Planet() {
     return (
         <View>
             <Text>Title</Text>
             <Text>Description</Text>
+            <Image></Image>
+            <SectionList 
+                // data lista, käyttöohje: https://reactnative.dev/docs/sectionlist
+            />
         </View>
     )
 }
