2c2a372d8ab4d18db5c1471361a02bf2a5f82bf6
n9puev00 <n9puev00@students.oamk.fi>
2021-11-09 17:16:54 +0200

    Planet view js

---
+++ components/Planet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Planet() {
+    return (
+        <View>
+            <Text>Title</Text>
+            <Text>Description</Text>
+        </View>
+    )
+}
