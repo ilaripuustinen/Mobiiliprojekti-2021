21d4112b2135317022ef74a035f6c960e1d7939f
n9puev00 <n9puev00@students.oamk.fi>
2021-11-09 18:02:28 +0200

    nytkö

--- components/Planet.js
+++ components/Planet.js
@@ -7,9 +7,7 @@ export default function Planet() {
             <Text>Title</Text>
             <Text>Description</Text>
             <Image></Image>
-            <SectionList 
-                // data lista, käyttöohje: https://reactnative.dev/docs/sectionlist
-            />
+            
         </View>
     )
 }
