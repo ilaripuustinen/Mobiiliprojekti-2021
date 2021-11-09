3d9305a7407d18b67432e34cb6b5ec1667b3785c
Ilari Puustinen <n9puil00@students.oamk.fi>
2021-11-09 17:55:28 +0200

    Frontpage/App

---
+++ components/frontpage.js
@@ -0,0 +1,11 @@
+import React, {View, Text} from 'react';
+import style from '../style/style';
+
+export default function Frontpage() {
+
+    return (
+    <div>
+       <h1>moi</h1>
+    </div>
+    )
+}
