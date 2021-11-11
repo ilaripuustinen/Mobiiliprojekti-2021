c7e5b239c4acb437720729b682d38adeb3a62b83
Met4 <n9liju00@students.oamk.fi>
2021-11-11 16:40:21 +0200

    Horoscope sivu vaiheessa

--- App.js
+++ App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Frontpage from './components/Frontpage';
 import Planet from './components/Planet';
 import style from './style/style';
+import Horoscope from './components/Horoscope';
 
 export default function App() {
 
   return (
     <NavigationContainer>
-    <Frontpage />
+      <Frontpage />
     </NavigationContainer>
   );
 }
