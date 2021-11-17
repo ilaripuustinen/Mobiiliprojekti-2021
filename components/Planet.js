b055e6e50883b8d20342630d02b2993af9d8a5b1
n9puev00 <n9puev00@students.oamk.fi>
2021-11-17 15:38:36 +0200

    SEND HELP

--- components/Planet.js
+++ components/Planet.js
@@ -6,31 +6,42 @@ export default function Planet() {
 
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    // const [planet, setPlanet] = useState([]);
+    // const [notPlanet, setNotPlanet] = useState([]);
     console.log(data);
   
     useEffect(() => {
-      fetch('https://api.le-systeme-solaire.net/rest/bodies?data=id,isPlanet,name,englishName')
+      fetch('https://api.le-systeme-solaire.net/rest/bodies?data=id,isPlanet,name,englishName,moons')
         .then((response) => response.json())
-        .then((json) => setData(json))
+        .then((json) => setData(json))    
         .catch((error) => console.error(error))
         .finally(() => setLoading(false));
     }, []);
-  
+
+/*     function singlePlanet() {
+      for (let i = 0; i < data; i++)
+        if (data.isPlanet === true) {
+          setPlanet();
+        } else {
+          setNotPlanet();
+        }
+    } */
+
     return (
   
       <View style={styles.container}>
-        {isLoading ? <Text>Loading...</Text> : 
-        ( <View style={styles.container}>
-            <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Planets:</Text>
-            <FlatList
-              data={data.bodies}
-              keyExtractor={({ id }) => id}
-              renderItem={({ item }) => (
-                <Text>{item.id + ', ' + item.name + ', ' + item.englishName}</Text>
-              )}
-            />
-          </View>
-        )}
-      </View>
+      {isLoading ? <Text>Loading...</Text> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Planets:</Text>
+          <FlatList
+            data={data.bodies}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.name + '. ' + item.englishName}</Text>
+            )}
+          />
+        </View>
+      )}
+    </View>
     );
-}
+};
