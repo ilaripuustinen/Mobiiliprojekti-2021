d42f7049c0b91f7ec77675eb40f0addbe4178167
n9puev00 <n9puev00@students.oamk.fi>
2021-11-17 16:27:02 +0200

    she works

--- components/Planet.js
+++ components/Planet.js
@@ -4,18 +4,26 @@ import styles from '../style/style';
 
 export default function Planet() {
 
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-    // const [planet, setPlanet] = useState([]);
-    // const [notPlanet, setNotPlanet] = useState([]);
-    console.log(data);
+    // const [isLoading, setLoading] = useState(true);
+    // const [data, setData] = useState([]);
+    const [planet, setPlanet] = useState([]);
   
     useEffect(() => {
       fetch('https://api.le-systeme-solaire.net/rest/bodies?data=id,isPlanet,name,englishName,moons')
         .then((response) => response.json())
-        .then((json) => setData(json))    
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
+        .then((json) => {
+          console.log(json.bodies);
+
+          const temp = Array();
+          json.bodies.forEach(element => {
+            if (element.isPlanet) {
+              console.log(element.name);
+              temp.push(element)
+            }
+          });
+          setPlanet(temp);
+        })
+        
     }, []);
 
 /*     function singlePlanet() {
@@ -28,20 +36,27 @@ export default function Planet() {
     } */
 
     return (
-  
+
       <View style={styles.container}>
-      {isLoading ? <Text>Loading...</Text> : 
-      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-          <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Planets:</Text>
-          <FlatList
-            data={data.bodies}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>{item.name + '. ' + item.englishName}</Text>
-            )}
-          />
-        </View>
-      )}
-    </View>
-    );
-};
+        <Text>Planets</Text>
+        {planet.map(planet => (
+          <Text>{planet.name}</Text>
+        ))}
+      </View>)
+   
+      // <View style={styles.container}>
+      // {isLoading ? <Text>Loading...</Text> : 
+      // ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+      //   <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10}}>Planets:</Text>
+      //    <FlatList
+    //         data={data.bodies}
+    //         keyExtractor={({ id }, index) => id}
+    //         renderItem={({ item }) => (
+    //           <Text>{item.name + '. ' + item.englishName}</Text>
+    //         )}
+    //       />
+    //     </View>
+    //   )}
+    // </View> }
+    // );
+  };
