317c384b9504987aa29bd5fe86f27a9eedecdaff
Ilari Puustinen <n9puil00@students.oamk.fi>
2021-11-26 15:26:28 +0200

    Jotain

--- components/Home.js
+++ components/Home.js
@@ -1,40 +1,41 @@
 import React from 'react';
-import { View, Text, Image, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, Image, Button, ImageBackground } from 'react-native';
 import styles from '../styles/styles';
 
 export default function Home({ navigation }) {
 
   return (
     <View style={styles.container}>
+       {/* <ImageBackground source={require("../assets/Images/map/Map.png")} resizeMode="cover" style={styles.background}> */}
       <Text style={styles.text}>Home screen</Text>
-        {/* <Button
-        style={styles.text}
+      <div>
+      <Button
           color={'transparent'}
           title="Planets"
           onPress={() => navigation.navigate('Planets')}
-        /> */}
-        <Button
-          color={'transparent'}
-          title="FetchApi"
-          onPress={() => navigation.navigate('FetchApi')}
         />
-        <Button
-          color={'transparent'}
-          title="Ilarin Demot"
-          onPress={() => navigation.navigate('Ilarin Demot')}
-        />
-        <Button
-          color={'transparent'}
-          title="Horoscope"
-          onPress={() => navigation.navigate('Horoscope')}
-        />
-        <Button
-          color={'transparent'}
-          title="Map"
-          onPress={() => navigation.navigate('Map')}
-        />
+        </div>
+      <Button
+        color={'transparent'}
+        title="FetchApi"
+        onPress={() => navigation.navigate('FetchApi')}
+      />
+      <Button
+        color={'transparent'}
+        title="Ilarin Demot"
+        onPress={() => navigation.navigate('Ilarin Demot')}
+      />
+      <Button
+        color={'transparent'}
+        title="Horoscope"
+        onPress={() => navigation.navigate('Horoscope')}
+      />
+      <Button
+        color={'transparent'}
+        title="Map"
+        onPress={() => navigation.navigate('Map')}
+      />
+      {/* </ImageBackground> */}
     </View>
   );
 }
---
+++ styles/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      textAlign: 'center',
+      paddingTop: '200px',
+      fontSize: 30,
+
+    },
+    text: {
+      fontFamily: 'Merriweather',
+      fontSize: 30,
+      color: 'black',
+    //   border: 1,
+    //   border: 'solid',
+    //   borderColor: 'black',
+    },
+    background: {
+        flex: 1,
+        blurRadius: 10,
+        justifyContent: "center"
+    },
+});
