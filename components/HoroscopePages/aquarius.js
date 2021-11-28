5d8d773864f01ff68a97430cbc25d5e6f96ffa0a
Met4 <n9liju00@students.oamk.fi>
2021-11-28 20:03:54 +0200

    horoskooppijuttuja

--- components/HoroscopePages/aquarius.js
+++ components/HoroscopePages/aquarius.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import { Text, View, ImageBackground } from 'react-native'
-import väliaikakuva from "../../assets/favicon.png"
+import aquariusImage from "../../assets/Images/map/Horoscope/Aquarius.png"
 import styles from '../../styles/styles';
 
 export default function aquarius() {
+
+    const [todayHS] = useState([]);
+    fetch("https://horoscope-api.herokuapp.com/horoscope/today/Aquarius")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    
+
     return (
         <View>
             <ImageBackground source={require("../../assets/HoroscopeBackground.png")} resizeMode="cover" style={styles.background}>
-            <div style={{ alignSelf: 'center', marginTop: 100 }}>
-                <img height={200} src={väliaikakuva}></img>
-            </div>
-            <div>
-                <h1 style={{textAlign: 'center', color: 'white'}}>Aquarius</h1>
+                <div style={{ alignSelf: 'center', marginTop: 100 }}>
+                    <img height={300} src={aquariusImage}></img>
                 </div>
+                <div>
+                    <h1 style={{ textAlign: 'center', color: 'white' }}>Aquarius</h1>
+                </div>
+                <Text style={styles.hstext}>Horoskooppi API teksti tulee tähän</Text>
                 <div style={{ paddingBottom: 800 }}>
-
                 </div>
             </ImageBackground>
         </View>
