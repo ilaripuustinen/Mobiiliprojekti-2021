80ba225dcde10de0067f0d7a2dc8b333bfa4645a
n9puev00 <n9puev00@students.oamk.fi>
2021-12-07 15:22:29 +0200

    prkl

--- components/News.js
+++ components/News.js
@@ -4,22 +4,7 @@ import { View, Image, Text, ScrollView } from 'react-native';
 export default function News() {
     const [item, setItem] = useState([]);
 
-    useEffect(() => {
-        fetch("https://space-news.p.rapidapi.com/news", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "space-news.p.rapidapi.com",
-                "x-rapidapi-key": "a510eb0a25mshc2d63cdae0e2100p14d0eajsnda18b271c9e3"
-            }
-        })
-            .then(response => {
-                console.log(response);
-                setItem(response);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    })
+
 
     return (
         <View>
