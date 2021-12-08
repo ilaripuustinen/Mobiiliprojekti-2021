8f58f2d3cbf49bc3369c48f977b96249fe10f5f2
Oskari Juntunen <n9juos00@students.oamk.fi>
2021-12-08 22:15:57 +0200

    mobiili aukeaa ehkä etusivu

--- styles/styles.js
+++ styles/styles.js
@@ -1,63 +1,80 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      textAlign: 'center',
-      justifyContent: 'center',
-/*       paddingTop: '200px', */
-      fontSize: 30,
-    },
-    text: {
-      fontFamily: 'RussoOne',
-      fontSize: 30,
-      color: '#DD2A2A',
-     /*  backgroundColor: '#16131D', */
-      padding: 5,
-      marginTop: 10,
-      marginBottom: 10,
-      borderBottomEndRadius: 20,
-      borderBottomLeftRadius: 20,
-      borderTopRightRadius: 20,
-      borderTopStartRadius: 20,
-      alignContent: 'center',
-      alignItems: 'center',
-    },
-    background: {
-        flex: 1,
-        // blurRadius: 10,
-        justifyContent: "center"
-    },
-    hstext: {
-      fontFamily: 'Merriweather',
-      fontSize: 30,
-      color: 'white',
-      textAlign: 'center',
-      opacity: 0.9,
-    },
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    /*       paddingTop: '200px', */
+    fontSize: 30,
+  },
+  text: {
+    fontFamily: 'RussoOne',
+    fontSize: 30,
+    color: '#DD2A2A',
+    /*  backgroundColor: '#16131D', */
+    padding: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomEndRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopStartRadius: 20,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    // blurRadius: 10,
+    justifyContent: "center"
+  },
+  hstext: {
+    fontFamily: 'Merriweather',
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+    opacity: 0.9,
+  },
 
-    frontpageLogo: {
-      flex: 1,
-      height: 100,
-      marginBottom: 40,
-      marginTop: 60
-    },
-    frontpageButton: {
-      flex: 1,
-      height: 110,
-      width: 260,
-      alignContent: 'center',
-    },
-    thumbnail: {
-      width: 250,
-      height: 250,
-    },
-    imageWrapper: {
-      alignItems: 'center',
-    },
-    newsImage: {
-      width: 350,
-      height: 350,
-      marginBottom: 10,
-    }
+  thumbnail: {
+    width: 250,
+    height: 250,
+  },
+  imageWrapper: {
+    alignItems: 'center',
+  },
+  newsImage: {
+    width: 350,
+    height: 350,
+    marginBottom: 10,
+  },
+  frontpageLogo: {
+    height: 100,
+    width: "auto",
+    marginBottom: 30,
+    marginTop: 80
+  },
+  frontpageButton: {
+    height: 130,
+    width: 280,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  frontpageContainer: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
+  },
+  frontpagePressable: {
+    flex: 1,
+    fontFamily: 'RussoOne',
+    fontSize: 30,
+    color: '#DD2A2A',
+    padding: 5,
+    marginTop: 30,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+
 });
